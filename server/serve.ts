f35ce99e42dab66b956a4b430e3fed006acf6cb3
YeongChang Yun <45313479+YunYeongChang@users.noreply.github.com>
2021-03-13 15:31:38 +0900

    implement : construct express based restful api server

--- server/serve.ts
+++ server/serve.ts
@@ -1,1 +1,27 @@
-console.log('this is serve file');
+import http from 'http';
+import { app } from './app';
+
+const port = process.env.PORT || '5000';
+
+app.set('port', port);
+const server = http.createServer(app);
+const onError = (error: { syscall: string; code: any }) => {
+  if (error.syscall !== 'listen') throw error;
+
+  switch (error.code) {
+    case 'EACCES':
+      throw new Error(`${port} requires elevated privileges`);
+    case 'EADDRINUSE':
+      throw new Error(`${port} is already in use`);
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  console.log(`Secured API Server is Running on ${port}`);
+};
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
