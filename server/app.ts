ac4f79048d36ad7feecf8f7b80a00682a5aabbde
YeongChang Yun <45313479+YunYeongChang@users.noreply.github.com>
2021-03-13 15:57:45 +0900

    implement : construct express based restful api server

---
+++ server/app.ts
@@ -0,0 +1,69 @@
+import * as path from 'path';
+import * as express_ejs from 'ejs';
+
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import { statSync } from 'fs';
+import { config } from 'dotenv';
+import { json, urlencoded } from 'express';
+import { connect, connection } from 'mongoose';
+
+import router from '../src/index';
+
+const app = express();
+let DBError: string | null,
+  DBAttempt: number = 0;
+
+//OPEN LOCAL ENV FILE WHEN NOT RUNNING IN CI/CD
+if (!process.env.RUN_IN_CI_CD) {
+  statSync(path.join(__dirname, '../env/.env'));
+  config({ path: path.join(__dirname, '../env/.env') });
+}
+
+app.use(cors());
+app.use(json({ limit: '10mb' }));
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', router);
+app.engine('html', express_ejs.renderFile);
+
+const delay = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+//#DATABASE CONNECT FUNCTION
+const DBConnect = (): void => {
+  connect(
+    `mongodb://${process.env.DB_USER_ID}:${process.env.DB_USER_PW}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=${process.env.DB_AUTH_USER_DB}`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+    .then(() => {
+      DBAttempt = 0;
+      DBError = null;
+      console.log(`[DB] Database connected on port ${process.env.DB_PORT}`);
+    })
+    .catch((err) => {
+      DBError = err.toString();
+      console.log(DBError);
+    });
+};
+
+connection.on(
+  'disconnected',
+  async (): Promise<void> => {
+    console.log(
+      `[DB] Database disconnected. Trying to reconnect... (${DBAttempt++})`
+    );
+    DBError = 'disconnected';
+    await delay(1000);
+    DBConnect();
+  }
+);
+
+DBConnect();
+export { app, DBError, delay };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-const hello = (word: string): string => {
-    return word;
-};
+import { Request, Response, Router } from 'express';
 
-console.log(hello('hello this is typescript!'));
+const router = Router();
+
+router.get('/', function (req: Request, res: Response) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<title>TodayTopic RestFul API Server</title>');
+  res.write(
+    '<link rel="icon" href="https://www.readlight.me/common/icon.png">'
+  );
+  res.write('Welcome!<br>This is API Server of TodayTopic<br><br>');
+  res.end('API Document is <a href="https://api.readlight.me/docs">HERE</a>');
+});
+
+export default router;
