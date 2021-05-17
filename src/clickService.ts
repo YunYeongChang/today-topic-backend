f2a8a56b27554e355df45ef4a3c56afd7c723257
YeongChang Yun <45313479+YunYeongChang@users.noreply.github.com>
2021-05-17 15:12:30 +0900

    implement : change system to mimacstudy event type

--- src/clickService.ts
+++ src/clickService.ts
@@ -1,12 +1,16 @@
 import { Request, Response, Router } from 'express';
 import moment from 'moment';
-import cron, { ScheduledTask } from 'node-cron';
+import cron from 'node-cron';
 
 let leftCount: number = 1,
   default_count: number = 1,
   winnerList: Array<String> = [],
-  checkSecond: number = 0;
+  check = {
+    minute: 0, // 00분에 작동하려면 0, 매분 check.second초에 작동하려면 60을 입력
+    second: 0,
+  };
 
+// # 이벤트 환경 설정
 const decreaseCount = (userID: string) => {
   winnerList.push(userID);
   leftCount = leftCount - 1;
@@ -16,18 +20,22 @@ const checkWinner = (userID: string) => {
   return winnerList.includes(userID);
 };
 
-const setEnvironment = (default_count: number) => {
+const setEnvironment = () => {
   winnerList = [];
   leftCount = default_count;
 };
 
+// # 이벤트 참여
 const clickRouter = Router(),
   clickHandler = async (req: Request, res: Response) => {
     if (req.query.userID != undefined) req.body.userID = req.query.userID;
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
     if (!req.body.userID) res.end('login');
-    else if (moment().seconds() >= checkSecond) {
+    else if (
+      moment().minutes() >= check.minute % 60 &&
+      moment().seconds() >= check.second % 60
+    ) {
       if (checkWinner(req.body.userID)) res.end('already');
       else if (leftCount > 0) {
         decreaseCount(req.body.userID);
@@ -43,6 +51,7 @@ const clickRouter = Router(),
 clickRouter.get('/event/click', clickHandler);
 clickRouter.post('/event/click', clickHandler);
 
+// # 이벤트 설정
 clickRouter.get(
   '/event/set/:key/:value',
   async (req: Request, res: Response) => {
@@ -51,27 +60,36 @@ clickRouter.get(
     if (Number.isNaN(Number(req.params.value)))
       return res.end(`Invalid Parameter Value Input`);
 
-    if (req.params.key === 'count') {
-      default_count = Number(req.params.value);
-      res.end(`New Item Count Setted to ${default_count}!`);
-    } else if (req.params.key === 'check') {
-      checkSecond = Number(req.params.value);
-      schedule.stop();
-      schedule = cron.schedule(
-        (checkSecond === 0 ? 59 : checkSecond - 1) + ' * * * * *',
-        scheduledTask
-      );
-      res.end(`Click-Check-Second Setted to ${checkSecond}!`);
-    } else res.end('Unexecutable Command Requested');
+    switch (req.params.key) {
+      case 'count':
+        default_count = Number(req.params.value);
+        return res.end(`New Item Count Setted to ${default_count}!`);
+      case 'second':
+        check.second = Number(req.params.value);
+        return res.end(`Click-Check-Second Setted to ${check.second}!`);
+      case 'minute':
+        check.minute = Number(req.params.value);
+        return res.end(`Click-Check-Minute Setted to ${check.minute}!`);
+      case 'winner':
+        if (Number(req.params.value) === 0) {
+          setEnvironment();
+          return res.end('Winner List has been cleared.');
+        }
+      default:
+        return res.end('Unexecutable Command Requested');
+    }
   }
 );
 
+// # 이벤트 정보 출력
 clickRouter.get('/event/info', async (req: Request, res: Response) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<title>Click Event Info</title>');
   res.write(
     '<link rel="icon" href="https://www.multiflex.tk/common/icon.png">'
   );
+  res.write(`<br> check minute : ${check.minute}`);
+  res.write(`<br> check second : ${check.second}`);
   res.write(`<br> left count : ${leftCount}`);
   res.write(`<br> new item count : ${default_count}`);
   res.write(`<br><br> winners : ${winnerList.length}`);
@@ -81,32 +99,32 @@ clickRouter.get('/event/info', async (req: Request, res: Response) => {
   res.end();
 });
 
-const scheduledTask = () => {
-  console.log(
-    `\n[Click Event] Winner List : ${winnerList.length === 0 ? 'none' : ''}`
-  );
-  winnerList.forEach((winner) => {
-    console.log(' -> ' + winner);
-  });
-  console.log(
-    `\n[Click Event] (${moment().format(
-      'yyyy/MM/DD HH:mm:ss.SSS'
-    )}) event rescheduled.`
-  );
-  setEnvironment(default_count);
-};
-
-let schedule: ScheduledTask = cron.schedule(
-  (checkSecond === 0 ? 59 : checkSecond - 1) + ' * * * * *',
-  scheduledTask
-);
-
+// # 이벤트 초기화 Cron Task
 cron.schedule('* * * * * *', () => {
   console.log(
     `[Click Event] (${moment().format(
       'yyyy/MM/DD HH:mm:ss.SSS'
-    )}) left : ${leftCount}  check : ${checkSecond}`
+    )})  check(minute: ${check.minute}, second: ${
+      check.second
+    })  /  left : ${leftCount} `
   );
+  if (
+    (check.minute === 60 && moment().seconds() === check.second) ||
+    (moment().minutes() === check.minute && moment().seconds() === check.second)
+  ) {
+    console.log(
+      `\n[Click Event] Winner List : ${winnerList.length === 0 ? 'none' : ''}`
+    );
+    winnerList.forEach((winner) => {
+      console.log(' -> ' + winner);
+    });
+    console.log(
+      `\n[Click Event] (${moment().format(
+        'yyyy/MM/DD HH:mm:ss.SSS'
+      )}) event rescheduled.`
+    );
+    setEnvironment();
+  }
 });
 
 export default {
